721ea4a67964f44fb4e5fba0f70f9acdb1c8296a
Jack Hage <frankunderwood@House.Of.Cards>
2016-12-05 11:13:19 +1100

    First push of this branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
@@ -9,4 +9,3 @@ module ApplicationHelper
     end
   end
 end
-
