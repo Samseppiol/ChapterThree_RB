936a0fc34312ec3a0483ea4f48cb3aec051f0aa2
Samseppiol <j_hage@live.com>
2016-12-11 19:21:31 +1100

    Changing of app controller

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,12 +11,20 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @user.name = "     "
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 
   test "email should be present" do
-    @user.email = " "
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+  test "email validation should accept valid addresses" do
+   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+   valid_addresses.each do |valid_address|
+     @user.email = valid_address
+     assert @user.valid?, "#{valid_address.inspect} should be valid"
+   end
+ end
 end
